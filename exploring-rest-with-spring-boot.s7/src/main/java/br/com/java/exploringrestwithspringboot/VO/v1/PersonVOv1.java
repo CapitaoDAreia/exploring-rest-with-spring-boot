087171143c480ffb62cc321aab0eaf194efb86d1
CapitaoDAreia <danieligord7@hotmail.com>
2023-08-26 16:07:05 -0300

    S12 - Custom JSON serialization

--- exploring-rest-with-spring-boot.s7/src/main/java/br/com/java/exploringrestwithspringboot/VO/v1/PersonVOv1.java
+++ exploring-rest-with-spring-boot.s7/src/main/java/br/com/java/exploringrestwithspringboot/VO/v1/PersonVOv1.java
@@ -1,8 +1,13 @@
 package br.com.java.exploringrestwithspringboot.VO.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender", })
 public class PersonVOv1 implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -16,12 +21,15 @@ public class PersonVOv1 implements Serializable {
 
     private Long id;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
     private String lastName;
 
     private String address;
 
+//    @JsonIgnore
     private String gender;
 
     public PersonVOv1(){}
