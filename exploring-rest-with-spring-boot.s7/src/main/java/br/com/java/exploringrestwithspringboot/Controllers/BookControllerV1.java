5f4b6472723af15eb797c68b375f05cd8197265f
CapitaoDAreia <danieligord7@hotmail.com>
2023-08-29 22:16:24 -0300

    S15 - Book challenge - implementing OpenAPI Swagger

--- exploring-rest-with-spring-boot.s7/src/main/java/br/com/java/exploringrestwithspringboot/Controllers/BookControllerV1.java
+++ exploring-rest-with-spring-boot.s7/src/main/java/br/com/java/exploringrestwithspringboot/Controllers/BookControllerV1.java
@@ -3,6 +3,12 @@ package br.com.java.exploringrestwithspringboot.Controllers;
 import br.com.java.exploringrestwithspringboot.Services.BookService;
 import br.com.java.exploringrestwithspringboot.Utils.MediaType;
 import br.com.java.exploringrestwithspringboot.VO.v1.BookVOv1;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,17 +17,38 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/book/v1")
+@Tag(name = "Books", description = "Endpoint to manage books")
 public class BookControllerV1 {
 
     @Autowired
     private BookService service;
 
     @GetMapping(value = "/{ID}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Find a book", description = "Find a book", tags = {"Books"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {@Content(schema = @Schema(implementation = BookVOv1.class))}),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+    })
     public BookVOv1 findById(@PathVariable(value = "ID") Long ID) {
         return this.service.findById(ID);
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Find all books", description = "Find all books", tags = {"Books"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = BookVOv1.class))
+                    )
+            }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+    })
     public List<BookVOv1> findAll() {
         return this.service.findAll();
     }
@@ -30,6 +57,12 @@ public class BookControllerV1 {
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
             consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
     )
+    @Operation(summary = "Create one book", description = "Define the request content type choosing between yml, json or xml", tags = {"Books"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {@Content(schema = @Schema(implementation = BookVOv1.class))}),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+    })
     public BookVOv1 create(@RequestBody BookVOv1 book) {
         return this.service.create(book);
     }
@@ -38,11 +71,25 @@ public class BookControllerV1 {
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
             consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
     )
+    @Operation(summary = "Update one book", description = "Update one book", tags = {"Books"}, responses = {
+            @ApiResponse(description = "Updated", responseCode = "200", content = {@Content(schema = @Schema(implementation = BookVOv1.class))}),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+    })
     public BookVOv1 update(@RequestBody BookVOv1 book) {
         return this.service.update(book);
     }
 
     @DeleteMapping(value = "/{ID}")
+    @Operation(summary = "Delete one book", description = "Delete one book", tags = {"Books"}, responses = {
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+    })
     public ResponseEntity<Object> delete(@PathVariable(value = "ID") Long ID) {
         this.service.delete(ID);
         return ResponseEntity.noContent().build();
