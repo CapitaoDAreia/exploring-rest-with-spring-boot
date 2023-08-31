2c46589ea4a64b6a1f6dd6ed8f184a219edb236a
CapitaoDAreia <danieligord7@hotmail.com>
2023-08-31 16:25:33 -0300

    S17 - JWT - UserRepository and UserServices

---
+++ exploring-rest-with-spring-boot.s7/src/main/java/br/com/java/exploringrestwithspringboot/Repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.java.exploringrestwithspringboot.Repositories;
+
+import br.com.java.exploringrestwithspringboot.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.userName =:userName")
+    User findByUsername(@Param("userName") String userName);
+}
